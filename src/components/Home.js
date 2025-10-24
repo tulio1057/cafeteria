01356b3b8ecdb2bda75171837b32f41da00dc21c
tulio1057 <tuliojfb283@gmail.com>
2025-10-24 11:18:01 -0300

    Teste de correção da animação das patinhas

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,23 @@
 import "../styles/home.css";
 import Cafe from "./Cafe";
 import Dicas from "./Dicas";
+import patinhaImg from "../assets/patinha.png";
 
 function Home() {
-  // Array para gerar 10 patinhas (você pode ajustar o número)
   const patinhas = Array.from({ length: 10 });
 
   return (
     <main className="home-container">
       <div className="patinhas-fundo">
         {patinhas.map((_, index) => (
-          <div
+         <div
             key={index}
             className="patinha-animada"
             style={{
-              // Posição vertical aleatória (entre 20% e 90% da tela)
-              top: `${20 + Math.random() * 70}vh`,
-              // Duração da animação (entre 10s e 20s)
-              animationDuration: `${10 + Math.random() * 10}s`,
-              // Atraso para que não comecem todas juntas (usa valores negativos)
-              animationDelay: `-${Math.random() * 15}s`,
+              backgroundImage: `url(${patinhaImg})`,
+              top: `${20 + Math.random() * 70}vh`, 
+              animationDuration: `${10 + Math.random() * 10}s`, 
+              animationDelay: `-${Math.random() * 15}s`, 
             }}
           />
         ))}
