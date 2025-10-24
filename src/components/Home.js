4bf8d53783f7689b8217ec58bf06b827d446c9d7
tulio1057 <tuliojfb283@gmail.com>
2025-10-24 11:25:05 -0300

    Remoção das patinhas

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,11 @@
 import "../styles/home.css";
 import Cafe from "./Cafe";
 import Dicas from "./Dicas";
-import patinhaImg from "../assets/patinha.png";
 
 function Home() {
-  const patinhas = Array.from({ length: 10 });
-
   return (
-    <main className="home-container">
-      <div className="patinhas-fundo">
-        {patinhas.map((_, index) => (
-         <div
-            key={index}
-            className="patinha-animada"
-            style={{
-              backgroundImage: `url(${patinhaImg})`,
-              top: `${20 + Math.random() * 70}vh`, 
-              animationDuration: `${10 + Math.random() * 10}s`, 
-              animationDelay: `-${Math.random() * 15}s`, 
-            }}
-          />
-        ))}
-      </div>
-
-      <div className="home-content">
+    <main>
+      <div>
         <Cafe />
         <Dicas />
       </div>
